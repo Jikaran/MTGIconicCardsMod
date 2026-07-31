b9c6a049eac7558cb53aed6f5a1b855a86438ba1
Jikaran <jikarantv@gmail.com>
2026-08-01 01:28:03 +0200

    Update v0.1.2
    Updated some Cardtext
    
    Added:
    New Card Sheoldred
    
    Known Issue:
    Sheoldred calls missing vfx error in log
    Brainstorm only retunrs first choosen card to deck.

---
+++ MTGIconicCards/MTGIconicCardsCode/Cards/Sheoldred.cs
@@ -0,0 +1,38 @@
+﻿using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MTGIconicCards.MTGIconicCardsCode.Cards;
+using MTGIconicCards.MTGIconicCardsCode.Powers;
+
+namespace MTGIconicCards.MTGIconicCardsCode.Cards;
+
+[Pool(typeof(ColorlessCardPool))]
+public class Sheoldred() : MTGIconicCardsCard(3,
+    CardType.Power, CardRarity.Rare,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+        [
+        new PowerVar<SheoldredPower>(1M)
+        ];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => 
+        [
+         CardKeyword.Ethereal
+        ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        Sheoldred sheoldred = this;
+        SheoldredPower sheoldredPower = await PowerCmd.Apply<SheoldredPower>(choiceContext, target: sheoldred.Owner.Creature, (Decimal) sheoldred.DynamicVars["SheoldredPower"].IntValue, sheoldred.Owner.Creature, (CardModel) sheoldred);
+    }
+
+    protected override void OnUpgrade() => this.AddKeyword(CardKeyword.Innate);
+}
---
+++ MTGIconicCards/MTGIconicCardsCode/Powers/SheoldredPower.cs
@@ -0,0 +1,36 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using MTGIconicCards.MTGIconicCardsCode.Powers;
+
+namespace MTGIconicCards.MTGIconicCardsCode.Powers;
+
+
+public class SheoldredPower() : MTGIconicCardsPower
+{
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+
+    public override async Task AfterCardDrawn(
+        PlayerChoiceContext choiceContext, 
+        CardModel card, 
+        bool fromHandDraw)
+    {
+        SheoldredPower sheoldredPower = this;
+        if (fromHandDraw)
+        {
+            return;
+        }
+        VfxCmd.PlayOnCreatureCenters((IEnumerable<Creature>) sheoldredPower.CombatState.HittableEnemies, "vfx/vfx_attack_slash");
+        SfxCmd.Play("slash_attack.mp3");
+        IEnumerable<DamageResult> damageResults = await CreatureCmd.Damage(choiceContext, (IEnumerable<Creature>) sheoldredPower.CombatState.HittableEnemies, (Decimal) sheoldredPower.Amount, ValueProp.Unpowered, sheoldredPower.Owner);
+        await CreatureCmd.Heal(sheoldredPower.Owner, (Decimal) sheoldredPower.Amount, true);
+    }
+}
